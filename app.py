aa6b81da3005a66148b450c88a82aae8f6bd1f68
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-10-19 12:02:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if source_radio == settings.IMAGE:
             boxes = res[0].boxes
             res_plotted = res[0].plot()[:, :, ::-1]
             st.image(res_plotted, caption='Detected Image',use_column_width=True)
-            st.expander("Detection Results"):
+            st.expander("Detection Results")
             for box in boxes:
                 st.write(box.data)
 else:
